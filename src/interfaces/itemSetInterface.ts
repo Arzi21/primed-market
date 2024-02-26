85f8e2032ee22e4e20e35a91103f924af64fa7a6
Arzi21 <aretlegland@gmail.com>
2024-02-27 00:03:45 +0100

    added interface, setup initial values for itemSets

---
+++ src/interfaces/itemSetInterface.ts
@@ -0,0 +1,6 @@
+
+
+export interface ItemSetInterface {
+    setName: string,
+    setItems: string[]
+}
--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,7 +1,15 @@
-
-import styles from "./main.module.css"
+import { useState } from "react";
+import { ItemSetInterface } from "../interfaces/itemSetInterface";
+import styles from "./main.module.css";
 
 export const Main = () => {
+
+    //temporary measure:
+    const exampleSet:ItemSetInterface[] = [{setName: "Survival mods", setItems: ["redirection", "vitality", "vigor", "steel_fiber"]}];
+    const [itemSet, setItemSet] = useState<ItemSetInterface[]>(exampleSet);
+
+    
+
     return(
         <main className={styles.main}>
             <p>testing main</p>
