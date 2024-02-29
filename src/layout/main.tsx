dd1d1f59a6869c0f2d5700caf769a511b58dcf81
Are Legland <70661069+Arzi21@users.noreply.github.com>
2024-02-29 10:07:59 +0100

    updated main to handle localstorage change. Added key to mappable template

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
-import { LocalStorageCall } from "../helpers/localStorageCall";
+import { InitialiseTestValue } from "../helpers/localStorageCalls";
 
 import { ItemSetCard } from "../components/itemSetCard";
 import styles from "./main.module.css";
 
 export const Main = () => {
 
-    const {itemSet} = LocalStorageCall();
+    const {itemSet} = InitialiseTestValue();
 
     return(
         <section className={styles.main}>
             <h2>testing main</h2>
             {itemSet.map((itemSetValues) => (
-                <Link to={"/set/" + itemSetValues.setNameUrl}>
+                <Link to={"/set/" + itemSetValues.setNameUrl} key={itemSetValues.setNameUrl}>
                     <ItemSetCard itemSet={itemSetValues}/>
                 </Link>
             ))}
