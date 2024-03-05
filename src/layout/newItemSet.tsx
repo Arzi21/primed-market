7b9488d673a10ba623b38e3739381175222a1d71
Are Legland <70661069+Arzi21@users.noreply.github.com>
2024-03-05 11:30:15 +0100

    added control form use state

--- src/layout/newItemSet.tsx
+++ src/layout/newItemSet.tsx
@@ -8,35 +8,18 @@ import styles from "./newItemSet.module.css";
 export const NewItemSet = () => {
 
     const [inputQuantity, setInputQuantity] = useState(1);
+    const [itemSet, setItemSet] = useState({
+        setName: "undefined",
+        setNameUrl: "undefined",
+        setItemUrls: ["undefined"],
+        setDescription: "undefined"
+    })
 
     function handleSubmit(formData:any) {
         formData.preventDefault();
 
-        const formValues = new FormData(formData.target);
-        const formObject = Object.fromEntries(formValues.entries());
 
-        let newItemName = "default";
-        let newItems:string[] = [];
-        let initalLoop = true;
-        
-        for (const [key,value] of Object.entries(formValues)) {
-            //https://stackoverflow.com/questions/32464948/mapping-object-to-key-value-string-in-one-line
-            if (initalLoop){
-                newItemName = value;
-                initalLoop = false;
-            } else {
-                newItems.push(key);
-            }
-        }
-
-        const unformattedNewItemSet = {
-            setName: newItemName,
-            setNameUrls: "to be added",
-            setItemUrls: newItems,
-            setDescription: "to be added",
-        }
-
-        PostLocalStorage("ItemSet", [unformattedNewItemSet]);
+        PostLocalStorage("ItemSet", itemSet);
 
         // currently this doesn't work.
         //need to get the values from the form, not the keys.
