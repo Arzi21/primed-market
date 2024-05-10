f4ec8c9df497d7611012a1c21e9c8cfa048fdb34
Arzi21 <aretlegland@gmail.com>
2024-05-10 15:37:59 +0200

    correctly fetching and displaying modsetItems

--- src/layout/itemSetHighlight.tsx
+++ src/layout/itemSetHighlight.tsx
@@ -1,5 +1,19 @@
+import { useParams } from "react-router-dom";
+
+import { ReadModListSet } from "../helpers/localStorageCalls";
 
 
 export const ItemSetHighlight = () => {
-    return <p> testing itemSetHighlight </p>
+
+    const urlParams = useParams();
+    const modSetName = urlParams.setName;
+    const modSet = ReadModListSet(modSetName!); //add failure handling
+
+
+
+    let template = modSet.setItemUrls.map((modItem:string, index:number) => (
+        <p key={modSetName + "_" + index + 1}> {modItem} </p>
+    ));
+
+    return template
 }
