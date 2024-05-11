82df984189b56b3b4ef9a3e1217d0b7963a7f0dd
Are Legland <70661069+Arzi21@users.noreply.github.com>
2024-05-11 16:09:06 +0200

    setup useEffect skeleton. make plan for api calls. must complete later

--- src/layout/itemSetHighlight.tsx
+++ src/layout/itemSetHighlight.tsx
@@ -1,13 +1,33 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import { ReadModListSet } from "../helpers/localStorageCalls";
+import { ItemSetInterface } from "../interfaces/itemSetInterface";
 
 
 export const ItemSetHighlight = () => {
 
     const urlParams = useParams();
     const modSetName = urlParams.setName;
-    const modSet = ReadModListSet(modSetName!); //add failure handling
+    const modSet:ItemSetInterface = ReadModListSet(modSetName!);
+
+    useEffect(() => {
+
+        const initialURL = "https://api.warframe.market/v1";
+        // const itemURL = "/items/" + modSet.;
+
+
+
+        let modSetItemUrls = modSet.setItemUrls
+        for (let modUrlFriendlyName of modSetItemUrls) {
+            console.log('test: ' + modUrlFriendlyName);
+            //create state for mod items,
+            //in this loop we call and bind api return to state
+            //use state to template the values into a table
+
+            //Bonus: trottle quantity of api calls to avoid overcalling
+        }
+    }, [modSet]);
 
 
 
@@ -15,5 +35,5 @@ export const ItemSetHighlight = () => {
         <p key={modSetName + "_" + index + 1}> {modItem} </p>
     ));
 
-    return template
+    return <> {template} </>
 }
