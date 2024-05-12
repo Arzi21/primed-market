a4b9075302f3a9142e38b2ef4823cc7ee3055605
Arzi21 <aretlegland@gmail.com>
2024-05-12 18:08:28 +0200

    ran into CORS policy issues, checkout commit

--- src/layout/itemSetHighlight.tsx
+++ src/layout/itemSetHighlight.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { ReadModListSet } from "../helpers/localStorageCalls";
@@ -7,25 +7,34 @@ import { ItemSetInterface } from "../interfaces/itemSetInterface";
 
 export const ItemSetHighlight = () => {
 
+    const [modApiSet, setModApiSet] = useState<any|null>()
+
     const urlParams = useParams();
     const modSetName = urlParams.setName;
     const modSet:ItemSetInterface = ReadModListSet(modSetName!);
 
     useEffect(() => {
 
-        const initialURL = "https://api.warframe.market/v1";
+        const initalApiUrl = "https://api.warframe.market/v1";
         // const itemURL = "/items/" + modSet.;
 
 
 
         let modSetItemUrls = modSet.setItemUrls
         for (let modUrlFriendlyName of modSetItemUrls) {
-            console.log('test: ' + modUrlFriendlyName);
+        // for (let modUrlFriendlyName of ['vitality']) {
             //create state for mod items,
             //in this loop we call and bind api return to state
             //use state to template the values into a table
 
             //Bonus: trottle quantity of api calls to avoid overcalling
+
+            console.log('test: ' + modUrlFriendlyName);
+            const apiAddress = initalApiUrl + "/items/" + modUrlFriendlyName;
+            console.log(apiAddress);
+            // fetch(apiAddress)
+            // .then(res => res.json())
+            // .then(data => setModApiSet(data.payload));
         }
     }, [modSet]);
 
