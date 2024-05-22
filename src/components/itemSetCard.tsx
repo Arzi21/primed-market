046d090cb2e733144fc0ae186563c002bb3d90e7
Arzi21 <aretlegland@gmail.com>
2024-05-23 00:46:59 +0200

    <add> created

--- src/components/itemSetCard.tsx
+++ src/components/itemSetCard.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { ItemSetInterface } from "../interfaces/itemSetInterface"
 import styles from "./itemSetCard.module.css";
+import animations from "../styles/animations.module.css"
 
 export const ItemSetCard = (props: {itemSet:ItemSetInterface, displayDelete:boolean, deleteFunc:any}) => {
 
     const {itemSet, displayDelete, deleteFunc} = props
 
     return (
-    <div className={styles.cardWrapper}>
+    <div className={`${styles.cardWrapper} + ${animations.initializedFadeIn}`}>
         {displayDelete && <button className={styles.deletionButton} onClick={()=> {deleteFunc(itemSet)}}> Delete </button>}
         <Link to={"/set/" + itemSet.setNameUrl} className={styles.cardLink}>
             <p> {itemSet.setItemUrls.length} </p>
