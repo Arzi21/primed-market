6e73550b5634924687fe8698bebb99b1191a8d1a
Are Legland <70661069+Arzi21@users.noreply.github.com>
2024-06-01 14:29:26 +0200

    edited delete button

--- src/components/itemSetCard.tsx
+++ src/components/itemSetCard.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { ItemSetInterface } from "../interfaces/itemSetInterface"
 import styles from "./itemSetCard.module.css";
-import animations from "../styles/animations.module.css"
+import animations from "../styles/animations.module.css";
 
 export const ItemSetCard = (props: {itemSet:ItemSetInterface, displayDelete:boolean, deleteFunc:any}) => {
 
@@ -9,7 +9,7 @@ export const ItemSetCard = (props: {itemSet:ItemSetInterface, displayDelete:bool
 
     return (
     <div className={`${styles.cardWrapper} + ${animations.initializedFadeIn}`}>
-        {displayDelete && <button className={styles.deletionButton} onClick={()=> {deleteFunc(itemSet)}}> Delete </button>}
+        {displayDelete && <button className={styles.deletionButton} onClick={()=> {deleteFunc(itemSet)}}> X </button>}
         <Link to={"/set/" + itemSet.setNameUrl} className={styles.cardLink}>
             <p> {itemSet.setItemUrls.length} </p>
             <h3> {itemSet.setName} </h3>
