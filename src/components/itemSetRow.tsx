4f04eac693d5423f548ee861520b551d7aa873f2
Arzi21 <aretlegland@gmail.com>
2024-06-04 23:27:06 +0200

    <feat>[itemSetRow]: moved api call from parent component to child component. this day children are more self sufficient

--- src/components/itemSetRow.tsx
+++ src/components/itemSetRow.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 
 
 export const ItemSetRow = ({itemUrl}:{itemUrl:string}) => {
 
+    const [modApiSet, setModApiSet] = useState<any|null>()
+
+
+    useEffect(() => {
+
+        const apicall = "http://localhost:4000";
+
+
+        // for (let modUrlFriendlyName of modSetItemUrls) {
+            //create state for mod items,
+            //in this loop we call and bind api return to state
+            //use state to template the values into a table
+
+            //Bonus: trottle quantity of api calls to avoid overcalling
+
+
+
+            //if we want to fetch this api we need to either disable cors, or use a proxy server to handle the dataflow.
+            //currently we'll be using a chrome extension to block cors requests. if this works it MUST be replaced (if we made a backend)
+            const apiAddress = apicall + "/items/" + itemUrl;
+            fetch(apiAddress)
+            .then(res => res.json())
+            .then(data => setModApiSet(data.payload));
+    }, []);
+    console.log(modApiSet);
+
+
     return <article>
-        <p> Table row </p>
+        <p> {itemUrl} </p>
     </article>
 }
--- src/layout/itemSetHighlight.tsx
+++ src/layout/itemSetHighlight.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { ItemSetRow } from "../components/itemSetRow";
@@ -8,41 +7,11 @@ import { ItemSetInterface } from "../interfaces/itemSetInterface";
 
 export const ItemSetHighlight = () => {
 
-    const [modApiSet, setModApiSet] = useState<any|null>()
-
     const urlParams = useParams();
     const modSetName = urlParams.setName;
     const modSet:ItemSetInterface = ReadModListSet(modSetName!);
 
-    useEffect(() => {
 
-        const apicall = "http://localhost:4000";
-        // const itemURL = "/items/" + modSet.;
-
-
-
-        let modSetItemUrls = modSet.setItemUrls
-        // for (let modUrlFriendlyName of modSetItemUrls) {
-        for (let modUrlFriendlyName of modSetItemUrls) {
-            //create state for mod items,
-            //in this loop we call and bind api return to state
-            //use state to template the values into a table
-
-            //Bonus: trottle quantity of api calls to avoid overcalling
-
-
-
-            //if we want to fetch this api we need to either disable cors, or use a proxy server to handle the dataflow.
-            //currently we'll be using a chrome extension to block cors requests. if this works it MUST be replaced (if we made a backend)
-            const apiAddress = apicall + "/items/" + modUrlFriendlyName;
-            fetch(apiAddress)
-            .then(res => res.json())
-            .then(data => setModApiSet(data.payload));
-        }
-    }, []);
-
-
-    console.log(modApiSet);
 
     let template = modSet.setItemUrls.map((setItemUrlName:string, index:number) => (
         <ItemSetRow key={modSetName + "_" + index + 1} itemUrl={setItemUrlName}/>
