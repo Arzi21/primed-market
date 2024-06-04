5b0fde0c2ba6c6f5285d2d6666c0861ee1756fec
Arzi21 <aretlegland@gmail.com>
2024-06-04 11:25:02 +0200

    <feat>[serverSetup]: added basic functionality to ensure server is running

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
 
+const express = require("express")
+const axios = require("axios")
+
+
+const app = express()
+const port = 4000
+
+const logger = function() {
+    console.log("Server listening to port " + port)
+}
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    logger()
+})
