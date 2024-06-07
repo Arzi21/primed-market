cfa1117e4c8f6426a1f6bfb6ff3d716595440749
Arzi21 <aretlegland@gmail.com>
2024-06-07 15:34:47 +0200

    <feat>[setRow template]: cleaned up test values in template

--- src/components/itemSetRow.tsx
+++ src/components/itemSetRow.tsx
@@ -3,47 +3,23 @@ import { SetRowInterface } from "../interfaces/setRowInterface";
 
 import style from "./itemSetRow.module.css";
 import buttonStyle from "../styles/button.module.css";
-import image from "../imgs/heroimg1.png";
 
 export const ItemSetRow = ({itemUrl}:{itemUrl:string}) => {
 
-    const mockData = {
-        img: "string",
-        quantity: 1,
-        orderCreator: "string",
-        modRankCurrent: 1,
-        modRankMax: 1,
-        onlineValue: 1,
-        maxValue: 1
-    };
-
-    const [modApiSet, setModApiSet] = useState<SetRowInterface|null>(mockData);
-
+    const [modApiSet, setModApiSet] = useState<SetRowInterface|null>();
 
     useEffect(() => {
 
         const apicall = "http://localhost:4000";
 
-
-        // for (let modUrlFriendlyName of modSetItemUrls) {
-            //create state for mod items,
-            //in this loop we call and bind api return to state
-            //use state to template the values into a table
-
-            //Bonus: trottle quantity of api calls to avoid overcalling
-
-
-
-            //if we want to fetch this api we need to either disable cors, or use a proxy server to handle the dataflow.
-            //currently we'll be using a chrome extension to block cors requests. if this works it MUST be replaced (if we made a backend)
-            const apiAddress = apicall + "/items/" + itemUrl + "/orders";
-            // fetch(apiAddress)
-            // .then(res => res.json())
-            // .then(data => setModApiSet(data.payload))
-            // .catch(err => console.log(err))
+        const apiAddress = apicall + "/items/" + itemUrl + "/orders";
+        fetch(apiAddress)
+        .then(res => res.json())
+        .then(data => setModApiSet(data.payload))
+        .catch(err => console.log(err))
     }, []);
-    console.log(modApiSet);
-
+    
+    
 
     return <>
         { modApiSet ? 
