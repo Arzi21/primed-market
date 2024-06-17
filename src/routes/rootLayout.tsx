65521f59e86fcea387928ec6f524c8e0b4b0934e
Arzi21 <aretlegland@gmail.com>
2024-06-18 00:05:28 +0200

    <feat>[modalsetup]: added min. req. styles. Added modal is root layout

--- src/routes/rootLayout.tsx
+++ src/routes/rootLayout.tsx
@@ -3,15 +3,19 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../layout/header";
 import { Footer } from "../layout/footer";
 import { NavigationBar } from "../layout/navigation";
+import { Modal } from "../components/modal";
 
 import "./rootLayout.module.css";
 
 export const RootLayout = () => {
     return (
         <div className="App">
+            <Modal/>
             <Header/>
             <NavigationBar/>
-            <main><Outlet/></main>
+            <main>
+                <Outlet/>
+            </main>
             <Footer/>
         </div>
     );
